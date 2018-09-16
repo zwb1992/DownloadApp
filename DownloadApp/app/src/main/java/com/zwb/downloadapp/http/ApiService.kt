67c9321bedf2f948ad2b8bac1f30f87a369e253f
zwb1992 <zwbfight@163.com>
2018-09-16 22:28:23 +0800

    下载大文件需要加上@Streaming

--- DownloadApp/app/src/main/java/com/zwb/downloadapp/http/ApiService.kt
+++ DownloadApp/app/src/main/java/com/zwb/downloadapp/http/ApiService.kt
@@ -4,10 +4,12 @@ import io.reactivex.Observable
 import okhttp3.ResponseBody
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Streaming
 import retrofit2.http.Url
 
 interface ApiService {
 
+    @Streaming
     @GET
     fun download(@Url URL:String,@Header("Range") range:String):Observable<ResponseBody>
 }
